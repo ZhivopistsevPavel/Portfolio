9290c3aa14110a3ad2b49a0aeea853cfd6fcec49
ZhivopistsevPavel <zhivopistsevpavel@gmail.com>
2022-01-20 22:26:20 +0300

    change all project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// let name = 'Pavel';
+// let age = 36; 
+
+// const header = document.querySelector(".header");
+
+// console.log(header);
+
+// window.addEventListener('scroll', function() {
+//     let scrollPos = window.scrollY;
+
+//     if(scrollPos > 0) {
+//         header.classList.add('red');
+//     } else {
+//         header.classList.remove('red');
+//     }       
+// });
+
+// name = prompt('как твоёимя?', '');
+// alert(name);
+
+
+
